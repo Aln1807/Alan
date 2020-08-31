1fb715c3fd4181650aeeea595b0d39603a10bf63
Aln1807 <69919511+Aln1807@users.noreply.github.com>
2020-08-31 14:49:48 -0500

    Create Codigo.js

---
+++ Codigo.js
@@ -0,0 +1,7 @@
+function saludo(){
+    alert('hola')
+    //prompt("hola ingresa tu nombre")
+    var nombre=prompt("Dime tu nombre");
+    alert('hola'+''+nombre);
+    //document.write(nombre);
+    }
